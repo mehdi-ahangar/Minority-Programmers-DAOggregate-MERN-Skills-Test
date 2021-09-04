3306dd403ee4710738b5369538d9840abed233b8
mehdi-ahangar <88955876+mehdi-ahangar@users.noreply.github.com>
2021-09-05 00:19:57 +0430

    Update index.js

--- server-backend/index.js
+++ server-backend/index.js
@@ -7,43 +7,17 @@ const daoRepo = require ('./dal/dao.repo');
 const bodyParser = require ('body-parser');
 const fileUpload = require('express-fileupload');
 
-
-//const path= require ('path');
-//const fs = require('fs');
-//app.use(bodyParser.urlencoded({ extended: true}));
-//app.use(bodyParser.urlencoded());
-
-//app.use(bodyParser.json());
-
-
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-/*app.use(fileUpload({
-    createParentPath: true
-}));*/
 
-
-//app.use(bodyParser.urlencoded({ extended: true }));
-
-//app.use("/");
-//app.use( express.bodyParser());
-//app.use(bodyParser.json());
 app.use( cors());
-//app.use(express.urlencoded());
-//app.use(/);
-//app.use(bodyParser.urlencoded());
-//app.use(express.json());
 
-//repoBase();
 
 // DAO route handler
 app.use('/', DaoRouter);
 app.use('/logo',express.static('data/Icon/'));  
-
-//app.use('/data/Icon/', express.static('data/Icon/')); 
-//app.use('/upload', DaoRouter);
 
 const port = process.env.PORT || 80;
 //const port = 80;
